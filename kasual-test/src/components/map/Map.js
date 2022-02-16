e8b9668d41816493313e96e410e72279b2ba7ef6
marin f <marin1511@hotmail.fr>
2022-02-16 23:41:34 +0100

    measurements
    measurements

--- kasual-test/src/components/map/Map.js
+++ kasual-test/src/components/map/Map.js
@@ -10,47 +10,43 @@ function Map() {
   const [countries, setCountries] = useState([]);
   const [countryData, setCountryData] = useState([]);
   const [center, setCenter] = useState([46, 2]);
-  const [locNumber, setLocNumber] = useState("10");
+  const [locNumber, setLocNumber] = useState("500");
   const [countrySelected, setcountrySelected] = useState("FR");
 
   // On récupère ici les infos d'un seul pays (toutes les localisations des capteurs de qualité d'air + mesures etc.), sur lequel on a cliqué dans le dropdown
-  useEffect(() => {
-    axios
-      .get(
-        `https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/locations?limit=${locNumber}&country=${countrySelected}`
-      )
-      .then((res) => setCountryData(res.data));
-
-    // On récupère ici les coordonnées du PAYS dans le fichier "countryLoc.js" en fonction du pays que nous avons choisi dans le dropdown
-    const locData = countryCodes?.filter(
-      (pays) => pays.alpha2 === countrySelected
-    )[0];
-
-    return () => console.log("");
-  }, [countrySelected, locNumber]); //On relance useEffect à chaque changement de pays ou de nombre d'emplacements voulus
+  // useEffect(() => {
+  //   const axiosGet = async () => {
+  //     const response = await axios.get(
+  //       `https://docs.openaq.org/v2/measurements?country=${countrySelected}&limit=${locNumber}`
+  //     );
+  //     setCountryData(response.data.results);
+  //   };
+  //   axiosGet();
+  // }, [countrySelected, locNumber]); //On relance useEffect à chaque changement de pays ou de nombre d'emplacements voulus
 
   //on récupère ici la liste de tous les pays que l'on va mettre dans le dropdown
-  useEffect(() => {
-    axios
-      .get(
-        "https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/countries"
-      )
-      .then((res) => setCountries(res.data));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/countries"
+  //     )
+  //     .then((res) => setCountries(res.data));
+  // }, []);
 
   // permet de recharger la map pour qu'elle s'affiche correctement
-  setTimeout(function () {
-    window.dispatchEvent(new Event("resize"));
-  }, 500);
+  // setTimeout(function () {
+  //   window.dispatchEvent(new Event("resize"));
+  // }, 0);
 
   return (
     <>
       {/* On crée ici le bouton dropdown qui nous permet de choisir le pays que l'on souhaite */}
       <div className="header">
-        <p className="sensorName">Nombre de capteurs</p>
+        <p className="sensorName">Nombre de mesures max: {locNumber}</p>
+
         <input
           type="range"
-          min="1"
+          min=""
           max="1200"
           rangevalue="500"
           onChange={(e) => setLocNumber(e.target.value)}
@@ -81,26 +77,34 @@ function Map() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {countryData.results?.map((location) => (
+          {/* {countryData?.map((location) => (
             <Marker
-              key={location.id}
+              // key={
+              //   location.coordinates.longitude +
+              //   "-" +
+              //   location.coordinates.latitude +
+              //   "-" +
+              //   location.parameter
+              //   // location.locationId +
+              //   // "-" +
+              //   // location.location +
+              //   // "-" +
+              //   // location.parameter
+              // }
               position={[
                 location.coordinates.latitude,
                 location.coordinates.longitude,
               ]}
             >
               <Popup>
-                Les données de la qualité de l'air à
-                <strong> {location.name}</strong> sont :
-                {location.parameters?.map((param) => (
-                  <li key={param.id}>
-                    {" "}
-                    Paramètre : {param.displayName}, Valeur : {param.lastValue}{" "}
-                  </li>
-                ))}
+                Le paramètre de qualité de l'air relevé ici est :
+                {location.parameter + " "} <br />
+                Sa valeur est de {location.value + " "}
+                {location.unit}
+                Valeur relevée le :{" "}
               </Popup>
             </Marker>
-          ))}
+          ))} */}
         </MapContainer>
       </div>
     </>
